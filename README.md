052300a77e70ec0ee096edb660c5f8d477408688
Bobby Peck <soldier.moth@gmail.com>
2016-11-24 00:00:18 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Paginator
+
+Two companion Chrome extensions that provide easy buttons for navigating paginated web pages by munging the URL
+"Up" finds the last number and increments it while "Down" finds the last number and decrements it
---
+++ up/up.js
@@ -0,0 +1,16 @@
+
+function strReverse(s)  {
+  return s.split('').reverse().join('');
+}
+// React when a browser action's icon is clicked.
+chrome.browserAction.onClicked.addListener(function(tab) {
+  var url = strReverse(tab.url)
+  url = url.replace(/(\d+)(.*)$/,function(_,num,remainder){
+    num = parseInt(strReverse(num),10)+1
+    return strReverse(""+num)+remainder;
+  })
+  url = strReverse(url)
+  if url != tab.url {
+    chrome.tabs.update(tab.id, {url:url});
+  }
+});
